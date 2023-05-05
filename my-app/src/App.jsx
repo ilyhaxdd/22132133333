3c6a89dc2efc1fb7522fa4219261b8d48c4cb219
ilyhaxdd <twi71ne@yandex.com>
2023-05-05 20:28:43 +0500

    добавил проект свой первый я да

---
+++ my-app/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='container'>
+    <h1 className='heading'>Hello react!</h1>
+    <ul className='list'>
+      <li className='element'>A</li>
+      <li className='element'>B</li>
+      <li className='element'>C</li>
+      <li className='element'>D</li>
+      <li className='element'>E</li>
+    </ul>
+    </div>
+  );
+}
+
+export default App;
